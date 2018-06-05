11b8f009e2df6d183d3ab6d48e510c549edc38e6
cailinfeng <cailinfeng@mobanker.com>
2018-06-05 21:06:30 +0800

    init

---
+++ src/main/java/org/lynn/DesignPattern.java
@@ -0,0 +1,18 @@
+package org.lynn;
+
+public class DesignPattern {
+    /**
+     * 先有业务场景，才有设计模式，设计模式始终为业务模型服务。不可生搬硬套。
+     * 提升代码可读性、可扩展性、维护成本、复杂的业务问题。
+     */
+
+    //工厂模式
+
+    /**
+     * 大规模，标准化，批量生产
+     */
+    static class FactoryDesign {
+
+    }
+
+}
