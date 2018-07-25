79150897ffb0a655b5d2e074261d6874098ff8d2
cailinfeng <cailinfeng@mobanker.com>
2018-07-25 11:57:37 +0800

    thread.join

--- spring/src/main/java/org/lynn/InitBean.java
+++ spring/src/main/java/org/lynn/InitBean.java
@@ -1,15 +1,14 @@
 package org.lynn;
 
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class InitBean implements InitializingBean {
 
-    public InitBean(){
+    public InitBean() {
         System.out.println("InitBean constructor execute...");
     }
 
-    public void initMethod(){
+    public void initMethod() {
         System.out.println("init method execute...");
     }
 
@@ -19,7 +18,19 @@ public class InitBean implements InitializingBean {
     }
 
     public static void main(String[] args){
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:applicationContext.xml");
-        InitBean initBean = (InitBean) context.getBean("initBean");
+        Thread t1 = new Thread(() -> {
+            System.out.println("First task started");
+            System.out.println("Sleeping for 2 seconds");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("First task completed");
+        });
+        Thread t2 = new Thread(() -> System.out.println("Second task completed"));
+        t1.start();
+//        t1.join(); //线程实例的方法join()方法可以使得一个线程在另一个线程结束后再执行，即t2等到t1执行结束后再执行
+        t2.start();
     }
 }
