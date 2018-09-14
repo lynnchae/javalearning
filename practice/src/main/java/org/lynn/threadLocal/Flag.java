af53d9f31257a2d851df0037ef1e2a29919e529e
cailinfeng <cailinfeng@mobanker.com>
2018-09-14 17:38:23 +0800

    @Slf4j 日志配置

---
+++ practice/src/main/java/org/lynn/threadLocal/Flag.java
@@ -0,0 +1,17 @@
+package org.lynn.threadLocal;
+
+import lombok.Data;
+
+/**
+ * Class Name : org.lynn.threadLocal
+ * Description :
+ *
+ * @author : cailinfeng
+ * Date : 2018/9/14 17:11
+ */
+@Data
+public class Flag {
+
+    private String signal;
+
+}
--- practice/src/main/java/org/lynn/threadLocal/ThreadLocalDemo.java
+++ practice/src/main/java/org/lynn/threadLocal/ThreadLocalDemo.java
@@ -1,5 +1,7 @@
 package org.lynn.threadLocal;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Class Name : org.lynn.threadLocal
  * Description :
@@ -7,14 +9,20 @@ package org.lynn.threadLocal;
  * @author : cailinfeng
  * Date : 2018/9/14 14:31
  */
+@Slf4j
 public class ThreadLocalDemo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Thread t1 = Thread.currentThread();
         ThreadLocal<String> tl1 = new ThreadLocal<>();
-        ThreadLocal<String> tl2 = new ThreadLocal<>();
+        ThreadLocal<Flag> tl2 = new ThreadLocal<>();
         tl1.set("test");
-        tl2.set("test");
+        Flag f = new Flag();
+        f.setSignal("signaled");
+        tl2.set(f);
+        log.info("tl2 before update is {} ", f.getSignal());
+        f.setSignal("signaled2");
+        log.info("tl2 after update is {} ", f.getSignal());
         System.out.println(t1.getName());
     }
 
